7e3f79805ce581781d4d7ae46f85f8493a84ea51
gabriel <140641244+gabrielralo@users.noreply.github.com>
2025-12-01 09:46:32 -0300

    nutrgbef

--- java/aleatorio.js
+++ java/aleatorio.js
@@ -1,6 +1,6 @@
 const nomes = ["Gabriel", "Eduardo", "Leo", "Douglas", "Thomaz"];
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()*lista.lenght);
-    return lista [posicao];
+    return lista[posicao];
 }
 export const nome = aleatorio(nomes)
